ed222b7a2ccee233eb742fa383ba750efce449bb
josevidmal <92562836+josevidmal@users.noreply.github.com>
2025-07-22 18:30:27 -0600

    Adding CoreConcept component with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -18,7 +19,17 @@ function Header() {
         going to build!
       </p>
     </header>
-  )
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
 }
 
 function App() {
@@ -26,7 +37,21 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title="Components"
+              description="The core UI building block." 
+              image={componentsImg}
+            />
+            <CoreConcept 
+              title="Props"
+            />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
